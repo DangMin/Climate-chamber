8dd23e4a5f3b9d26415a06fb57b193242f17dc19
DangMin <nhatdang.min@gmail.com>
2017-06-22 16:51:21 +0300

    Add Stylus to config file. Error remaining.

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 const path = require('path')
 const Webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-const srcPath = path.resolve(__dirname, 'src')
-const outputPath = path.resolve(__dirname, 'public')
+const srcPath = path.resolve(__dirname, 'src/js')
+const outputPath = path.resolve(__dirname, 'public/dist')
 
 module.exports = {
   context: srcPath,
   entry: {
-    index: './index.js'
+    'index': './index.js',
+    'style': '../css/style.styl'
   },
   output: {
     path: outputPath,
@@ -16,7 +18,12 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.es6$/, exclude: /node_modules/, loader: "babel-loader" }
+      { test: /\.es6$/, exclude: /node_modules/, loader: "babel-loader" },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' }) },
+      { test: /\.styl$/, loader: ExtractTextPlugin.extract({ fallback: 'stylus', use: 'css-loader!stylus-loader?paths=node_modules/boostrap-stylus/stylus/' }) },
     ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin("./css/style.css"),
+  ]
 }
