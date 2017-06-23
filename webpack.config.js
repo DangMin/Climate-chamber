03a61ec622aea591c3e1e2f885689b2ddc7d6918
DangMin <nhatdang.min@gmail.com>
2017-06-23 23:47:08 +0300

    Fix error. Install Webpack-Dashboard, ExtractTextPlugin for Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const Webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const DashboardPlugin = require('webpack-dashboard/plugin')
 
 const srcPath = path.resolve(__dirname, 'src/js')
 const outputPath = path.resolve(__dirname, 'public/dist')
@@ -9,21 +10,39 @@ module.exports = {
   context: srcPath,
   entry: {
     'index': './index.js',
-    'style': '../css/style.styl'
+    'style': '../css/style.styl',
   },
   output: {
     path: outputPath,
     filename: "[name].js",
-    publicPath: "/public/"
   },
   module: {
     rules: [
       { test: /\.es6$/, exclude: /node_modules/, loader: "babel-loader" },
-      { test: /\.css$/, loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' }) },
-      { test: /\.styl$/, loader: ExtractTextPlugin.extract({ fallback: 'stylus', use: 'css-loader!stylus-loader?paths=node_modules/boostrap-stylus/stylus/' }) },
+      { test: /\.css$/, use: [
+        { loader: 'style-loader' },
+        { loader: 'css-loader' }
+      ] },
+      {
+        test: /\.styl$/,
+        loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader!stylus-loader' })
+      },
+      { test: /\.(png|jpg|gif|svg|ttf)$/, loader: 'url-loader'}
     ]
   },
   plugins: [
     new ExtractTextPlugin("./css/style.css"),
-  ]
+    new DashboardPlugin()
+  ],
+  resolve: {
+    extensions: ['.js'],
+    modules: [srcPath, "node_modules"],
+    //mainFields: ["loader", "main"]
+  },
+  resolveLoader: {
+    //extensions: ['js'],
+    modules: ["node_modules"],
+    //mainFields: ["loader", "main"]
+  },
+  watch: true
 }
